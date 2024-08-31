f5e2944688d0d776b6823c7a94d8209f823e8b3a
Hasitha <hasithawijewardhana19@gmail.com>
2024-08-31 09:04:24 -0700

    initialize car.java and main method

---
+++ src/Car.java
@@ -0,0 +1,28 @@
+public class Car {
+    int no;
+    String Name;
+
+    public Car() {
+    }
+
+    public Car(int no, String name) {
+        this.no = no;
+        Name = name;
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}
