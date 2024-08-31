3c61102c71a46f4216e9cece70be1b19e70908de
Hasitha <hasithawijewardhana19@gmail.com>
2024-08-31 09:17:16 -0700

    update main method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car car1 = new Car(1234,"Toyota");
+        car1.getName();
+        car1.getNo();
     }
 }
