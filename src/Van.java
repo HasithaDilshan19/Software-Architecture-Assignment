69a13c102420a346b094b62f3fa348e4d36824b3
Hasitha <hasithawijewardhana19@gmail.com>
2024-08-31 09:29:23 -0700

    add Van.java feature

---
+++ src/Van.java
@@ -0,0 +1,28 @@
+public class Van {
+    int no;
+    String Name;
+
+    public Van() {
+    }
+
+    public Van(int no, String name) {
+        this.no = no;
+        Name = name;
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}
